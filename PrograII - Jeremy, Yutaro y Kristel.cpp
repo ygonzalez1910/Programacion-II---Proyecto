ebd8918a45e8dc8a414e10770e6d6c4bd9900ac2
Yuli Gonzalez <ygonzalez1910@gmail.com>
2023-04-24 20:10:54 -0600

    primerCommit

---
+++ PrograII - Jeremy, Yutaro y Kristel.cpp
@@ -0,0 +1,12 @@
+// PrograII - Jeremy, Yutaro y Kristel.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Prueba";
+	cout << "Segunda prueba: " << endl;
+}
+
